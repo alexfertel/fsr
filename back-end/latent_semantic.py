ea977e9d20e0c81c5d09aadc53089a27c1fcebbf
Hieu Do Ngoc <hieudn.uh@gmail.com>
2019-11-01 13:06:17 -0400

    Add placeholder for Latent Semantic Model

---
+++ back-end/latent_semantic.py
@@ -0,0 +1,19 @@
+class LatentSemanticIRS:
+   index_table = None
+   
+   def __init__(self, parameter_list):
+      pass
+   
+   def index(self, parameter_list):
+      pass
+
+   def query(self, parameter_list):
+      pass
+
+   def store_index(self, file_path):
+      # OPTIONAL
+      pass
+
+   def load_index(self, file_path):
+      # OPTIONAL
+      pass
