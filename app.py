058c9c2fcf522ac83ebc72ae45f351ee755ad5d1
Hieu Do Ngoc <hieudn.uh@gmail.com>
2019-11-02 19:31:07 -0400

    Implement app's back-end

---
+++ app.py
@@ -0,0 +1,30 @@
+import eel
+from src.fsr_api import FileSystemRetrieval
+from src.latent_semantic import LatentSemanticModel
+
+
+retrieval_system = None
+
+@eel.expose
+def init_app():
+   use_lsi_model()
+
+@eel.expose
+def use_vector_model():
+   global retrieval_system
+   # retrieval_system = FileSystemRetrieval(VECTOR MODEL HERE)
+
+@eel.expose
+def use_lsi_model():
+   global retrieval_system
+   retrieval_system = FileSystemRetrieval(LatentSemanticModel(80))
+   
+@eel.expose
+def change_directory(new_dir):
+   global retrieval_system
+   # TODO: Validate directory !?
+   retrieval_system.index_directory(new_dir)
+
+@eel.expose
+def query(keywords):
+   return retrieval_system.query_directory(keywords)
