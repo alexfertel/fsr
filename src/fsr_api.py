3798e49f30e5179f0fd6b48e7832594d85deeb48
Hieu Do Ngoc <hieudn.uh@gmail.com>
2019-11-02 14:52:51 -0400

    Add fsr_api.py placeholder

--- src/fsr_api.py
+++ src/fsr_api.py
@@ -1,2 +1,13 @@
-from .latent_semantic import LatentSemanticModel
+class FileSystemRetrieval:
+   'Information Retrieval System using local file system as database'
 
+   def __init__(self, retrieval_model):
+      self.retrieval_model = retrieval_model
+
+   def set_directory(self, path):
+      'Change the current directory and index the documents inside'
+      pass
+
+   def query_current_dir(self, keywords):
+      'TODO'
+      pass
